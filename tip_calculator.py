3cc41ad110bdbf2d05d0f5b7760ae795f356666f
Ashish S. Kelkar <akelkar@gmail.com>
2014-03-06 22:38:42 -0800

    tip calc commit

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,4 +1,6 @@
-meal = 44.56
+meal = float(raw_input("How much was the cost of your meal without tax and tip? "))
+
+#meal = float(input_meal)
 tax = 8.35/100
 tip = 15.0/100
 
@@ -8,7 +10,7 @@ tip_value = meal_with_tax * tip
 
 total = meal_with_tax + tip_value
 
-print " the base cost of your meal is $",("%.2f" % meal)
-print " the tax on your meal is $",("%.2f" %tax_value)
-print " the tip on this meal is $",("%.2f" %tip_value)
-print " the total cost of your meal is $",("%.2f" %total)
+print " the base cost of your meal is :  $",("%.2f" % meal)
+print " the tax on your meal is       :  $",("%.2f" %tax_value)
+print " the tip on this meal is       :  $",("%.2f" %tip_value)
+print " the total cost of your meal is:  $",("%.2f" %total)
