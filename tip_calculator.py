24c9ec5e03fdfcdbc5525c8a70d8f8f1e957adba
Ashish S. Kelkar <akelkar@gmail.com>
2014-03-07 14:28:08 -0800

    tip calc refactor

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,12 +1,22 @@
-meal = float(raw_input("How much was the cost of your meal without tax and tip? "))
+import sys
+from sys import argv
+
+
+meal = float(sys.argv[1])
+tip = float(sys.argv[2])
+tax = float(sys.argv[3])
+ 
+
+
+#meal = float(raw_input("How much was the cost of your meal without tax and tip? "))
 
 #meal = float(input_meal)
-tax = 8.35/100
-tip = 15.0/100
+#tax = 8.35/100
+#tip = 15.0/100
 
-tax_value = meal * tax
+tax_value = meal * tax/100
 meal_with_tax = meal + tax_value
-tip_value = meal_with_tax * tip
+tip_value = meal_with_tax * tip/100
 
 total = meal_with_tax + tip_value
 
